67f6d7eba3d4376297266208fec219a072cb01e9
Luis DeLeon <luismdeleon@gmail.com>
2020-12-18 12:34:44 -0800

    trying out svelte using snowpack

---
+++ svelte-snowpack/snowpack.config.js
@@ -0,0 +1,28 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    /* ... */
+    '@snowpack/plugin-svelte'
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
