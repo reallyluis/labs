5d0d0ec6516ef21f73d68168ae109e9dfced5b8e
Luis <reallyluis@gmail.com>
2021-03-02 15:20:19 -0800

    add Transaction

---
+++ block-chain/components/Transaction.ts
@@ -0,0 +1,13 @@
+class Transaction {
+  constructor(
+    public amount: number,
+    public payer: string,
+    public payee: string,
+  ) {}
+
+  toString() {
+    return JSON.stringify(this);
+  }
+}
+
+export default Transaction;
