5a437f45e90fa97eabc8f7af5aff76f526066abc
Luis <reallyluis@gmail.com>
2021-03-06 07:04:40 -0800

    adding more basics

--- basics/index.js
+++ basics/index.js
@@ -4,11 +4,32 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
-if (hatPrice == bootsPrice) {
+if (hatPrice === bootsPrice) {
   console.log("Prices are the same");
 } else {
   console.log("Prices are different");
 }
 
-let totalPrice = hatPrice + bootsPrice;
-console.log(`Total Price: ${totalPrice}`);
+function sumPrices(...numbers) {
+  return numbers.reduce((total, val) => {
+    return total + (Number.isNaN(Number(val)) ? 0 : Number(val));
+  }, 0);
+}
+
+let totalPrice = sumPrices(hatPrice, bootsPrice);
+console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
+
+totalPrice = sumPrices(100, 200, 300);
+console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
+
+totalPrice = sumPrices(100, 200);
+console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
+
+let myVariable = "Adam";
+console.log(`Type: ${typeof myVariable}`);
+myVariable = 100;
+console.log(`Type: ${typeof myVariable}`);
+
+let firstCity;
+let secondCity = firstCity || "London";
+console.log(`City: ${secondCity}`);
