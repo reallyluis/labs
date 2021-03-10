1d06f3f324fdfb3270f83603e2e51101c19c3fac
Luis <349489+reallyluis@users.noreply.github.com>
2021-03-10 10:18:12 -0800

    add 'addTodo'

--- todoit-v1/todo-it.ts
+++ todoit-v1/todo-it.ts
@@ -1,5 +1,23 @@
 console.log("TodoIt");
 
 const todoList: string[] = [];
-
 console.log("Current todo list: ", todoList);
+
+const todoInput: HTMLInputElement = document.getElementById('todoInput') as HTMLInputElement;
+function addTodo(): void {
+  if (todoInput === null) {
+    console.log('The todo input is missing from the page!');
+    return;
+  }
+
+  const newTodo: string = todoInput.value;
+
+  if ('' !== newTodo.trim()) {
+    console.log('Adding todo: ', newTodo);
+
+    todoList.push(newTodo);
+    console.log('New todo list: ', todoList);
+
+    todoInput.value = '';
+  }
+}
