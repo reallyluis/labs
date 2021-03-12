11fca75ee62a8121d17a34ae226e1dc67d5b1e79
Luis <349489+reallyluis@users.noreply.github.com>
2021-03-11 17:36:30 -0800

    add socket.io

--- simple-chat/index.js
+++ simple-chat/index.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/*', (req, res) => {
   if (req.url) {
@@ -11,6 +12,10 @@ app.get('/*', (req, res) => {
   } else {
     res.sendFile(__dirname + '/index.html');
   }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 http.listen(3000, () => {
