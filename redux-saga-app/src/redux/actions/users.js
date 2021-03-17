11358a663110d113c88c3759952173107ac2a68c
Luis <349489+reallyluis@users.noreply.github.com>
2021-03-17 15:00:14 -0700

    add and setup redux-saga user

--- redux-saga-app/src/redux/actions/users.js
+++ redux-saga-app/src/redux/actions/users.js
@@ -1,8 +1,7 @@
 import * as type from '../types';
 
-export function getUsers(users) {
-  return { 
-    type: type.GET_USERS,
-    payload: users,
+export function getUsers() {
+  return {
+    type: type.GET_USERS_REQUESTED,
   }
 }
--- redux-saga-app/src/redux/store.js
+++ redux-saga-app/src/redux/store.js
@@ -1,8 +1,14 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/index.js';
+import rootSaga from './sagas/index';
 
+const sagaMiddleware = createSagaMiddleware();
 const store = compose(
+  applyMiddleware(sagaMiddleware),
   window.devToolsExtension && window.devToolsExtension(),
 )(createStore)(rootReducer);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
