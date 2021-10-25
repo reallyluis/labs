50d0c65cf0322ee6d740fb3fe2badba9cead77c5
Luis <349489+reallyluis@users.noreply.github.com>
2021-10-25 09:13:48 -0700

    init setup of next markdown exercise

---
+++ next-markdown-blog/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Dev Blog</title>
+      </Head>
+
+      <h2>Hello</h2>
+    </div>
+  )
+}
