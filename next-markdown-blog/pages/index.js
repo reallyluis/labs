00afb811462dba041ee88972b9b846d30ce561d0
Luis <349489+reallyluis@users.noreply.github.com>
2021-10-25 14:16:15 -0700

    save progress to next markdown exercise

--- next-markdown-blog/pages/index.js
+++ next-markdown-blog/pages/index.js
@@ -1,13 +1,49 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <Head>
         <title>Dev Blog</title>
       </Head>
 
-      <h2>Hello</h2>
+      <div className="posts">
+        {posts.map((post, index) => (
+          <h3 key={index}>
+            {post.frontmatter.title}
+          </h3>
+        ))}
+      </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  // Get files from the posts directory
+  const files = fs.readdirSync(path.join('posts'))
+
+  // Get slug and frontmatter from posts
+  const posts = files.map(file => {
+    // Create slug
+    const slug = file.replace('.md', '')
+
+    // Get frontmatter
+    const markdownWithMeta = fs.readFileSync(path.join('posts', file), 'utf-8')
+
+    const {data:frontmatter} = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter,
+    }
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
---
+++ next-markdown-blog/posts/test.md
@@ -0,0 +1,14 @@
+---
+title: 'Test Post'
+date: 'June 24 2021'
+excerpt: 'This is the excerpt'
+cover_image: '/images/posts/img1.jpg'
+---
+
+## Heading Two
+
+- Item 1
+- Item 2
+- Item 3
+
+Hello world
